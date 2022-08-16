db5e9c17f75c13740cc11ad58e429f0980068e7a
happbob <limms1217@naver.com>
2022-08-16 14:47:33 +0900

    🔥 add sentry code & update gitignore

--- api-message-producer/src/main.ts
+++ api-message-producer/src/main.ts
@@ -3,9 +3,13 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import * as expressBasicAuth from 'express-basic-auth';
 import { HTTPLoggingInterceptor } from 'common/logger/logger.interceptor';
+import * as Sentry from '@sentry/node';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+  })
   app.useGlobalInterceptors(new HTTPLoggingInterceptor());
   app.use(
     ['/docs', '/docs-json'],
